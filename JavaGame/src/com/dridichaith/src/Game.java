7247158187e7fc5b02673b70db91b2468980fab5
sethgreen23 <dridi.chaith@gmail.com>
2020-12-31 13:40:07 +0100

    make alot of anemy using controller class

--- JavaGame/src/com/dridichaith/src/Game.java
+++ JavaGame/src/com/dridichaith/src/Game.java
@@ -13,16 +13,25 @@ import javax.swing.Timer;
 
 import com.dridichaith.src.objects.Enemy;
 import com.dridichaith.src.objects.Player;
+import com.dridichaith.src.controller.Controller;
 import com.dridichaith.src.input.KeyInput;
 public class Game extends JPanel implements ActionListener{
 	Timer timer;
 	Player player;
-	Enemy enemy;
+	Controller c;
 	public Game() {
 		setFocusable(true);
 		player = new Player(100,100);
 		addKeyListener(new KeyInput(player));
-		enemy = new Enemy(100,200);
+		c = new Controller();
+		c.add(new Enemy(100,60));
+		c.add(new Enemy(300,140));
+		c.add(new Enemy(400,200));
+		c.add(new Enemy(0,0));
+		c.add(new Enemy(150,240));
+		c.add(new Enemy(200,290));
+		c.add(new Enemy(350,350));
+		c.add(new Enemy(500,400));
 		timer = new Timer(10,this);
 		timer.start();
 		
@@ -34,7 +43,7 @@ public class Game extends JPanel implements ActionListener{
 		g2d.drawImage(getBackgroundImage(), 0, 0, null);
 		//draw the player
 		player.draw(g2d);
-		enemy.draw(g2d);
+		c.draw(g2d);
 	}
 	
 	public Image getBackgroundImage() {
@@ -45,7 +54,7 @@ public class Game extends JPanel implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		player.update();
-		enemy.update();
+		c.update();
 		repaint();
 	}
 
--- JavaGame/src/com/dridichaith/src/controller/Controller.java
+++ JavaGame/src/com/dridichaith/src/controller/Controller.java
@@ -1,5 +1,38 @@
 package com.dridichaith.src.controller;
 
-public class Controller {
+import java.awt.Graphics2D;
+import java.util.LinkedList;
 
+import com.dridichaith.src.objects.Enemy;
+
+public class Controller {
+	private LinkedList<Enemy> enemies;
+	
+	public Controller() {
+		enemies= new LinkedList<Enemy>();
+	}
+	
+	public void add(Enemy e) {
+		enemies.add(e);
+	}
+	
+	public void draw(Graphics2D g2d) {
+		for(Enemy e : enemies) {
+			e.draw(g2d);
+		}
+	}
+	
+	public void  update() {
+		for(Enemy e : enemies) {
+			e.update();
+		}
+	}
+	
+	public void removeEnemy(Enemy e) {
+		enemies.remove(e);
+	}
+	
+	public LinkedList<Enemy> getEnemyCollection(){
+		return enemies;
+	}
 }
